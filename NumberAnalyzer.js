95fa9ff5d1b9395da9f727ccc67c0926da17d25b
Kayode <drkay4sure@gmail.com>
2024-05-26 13:53:42 +0100

    number analyzer done

--- NumberAnalyzer.js
+++ NumberAnalyzer.js
@@ -1,13 +1,15 @@
 const numbers1 = [15, 22, 87, 41, 120]; 
 const numbers2 = [17, 18, 35, 46, 49, 78, 99, 115, 136];
 
+// Number Analyzer function
 function numberAnalyzer (numbers) {
   
-
+    //For loop
     for (var i = 0; i < numbers.length; i++) {
         
         let number = numbers[i];
-    
+        
+        //If-else statement
         if (number <= 100) {
             if (number % 2 === 0) {
                 console.log(`${number} is even`);
@@ -26,5 +28,5 @@ function numberAnalyzer (numbers) {
 
 }
 
-
+//Invoke function
 numberAnalyzer(numbers2);
