a642a362c3f1bab44c10610fd817cbb76f2a696d
Kayode <drkay4sure@gmail.com>
2024-05-26 13:41:38 +0100

    number analyzer adjusted

--- NumberAnalyzer.js
+++ NumberAnalyzer.js
@@ -1,21 +1,30 @@
-const numbers = [15, 22, 87, 41, 120]; 
+const numbers1 = [15, 22, 87, 41, 120]; 
+const numbers2 = [17, 18, 35, 46, 49, 78, 99, 115, 136];
 
-for (var i = 0; i < numbers.length; i++) {
-    
-    let number = numbers[i];
+function numberAnalyzer (numbers) {
+  
 
-    if (number <= 100) {
-        if (number % 2 === 0) {
-            console.log(`${number} is even`);
-        }
+    for (var i = 0; i < numbers.length; i++) {
+        
+        let number = numbers[i];
     
-        else if(number % 2 === 1) {
-            console.log(`${number} is odd`);
+        if (number <= 100) {
+            if (number % 2 === 0) {
+                console.log(`${number} is even`);
+            }
+        
+            else if(number % 2 === 1) {
+                console.log(`${number} is odd`);
+            }
         }
+        
+       else{
+        console.log(`${number} is greater than 100`);
+       }
+        
     }
-    
-   else{
-    console.log(`${number} is greater than 100`);
-   }
-    
+
 }
+
+
+numberAnalyzer(numbers2);
